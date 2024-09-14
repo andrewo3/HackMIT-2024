a0955b836d6a0ed149512cb7014fd72a3d98f3d5
Andrew Ogundimu <andrew.ogundimu@gmail.com>
2024-09-14 12:45:36 -0400

    UI test

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from window import Window
+import asyncio
+import pygame
+from pygame_gui.elements import *
 
-def run():
-    Window(100,100)
+async def main():
+    win = Window(0.5,0.5,"Test")
+    manager = win.get_manager()
+    hello_button = UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                             text='Say Hello',
+                                             manager=manager)
+    while win.running:
+        win.draw()
+        win.update()
+        await asyncio.sleep(0)
     
 if __name__ == "__main__":
-    run()
+    asyncio.run(main())
--- window.py
+++ window.py
@@ -1,5 +1,27 @@
 import pygame
+import pygame_gui
 
 class Window():
-    def __init__(self,w,h):
-        self.win = pygame.display.set_mode((w,h))
+    def __init__(self,w,h,title="new window"):
+        pygame.init()
+        desktop = (pygame.display.Info().current_w,pygame.display.Info().current_h)
+        scaled_desktop = (int(desktop[0]*w),int(desktop[1]*h))
+        self.win = pygame.display.set_mode(scaled_desktop)
+        pygame.display.set_caption(title)
+        self.ui_manager = pygame_gui.UIManager(scaled_desktop,theme_path="static/theme.json")
+        self.clock = pygame.time.Clock()
+        self.running = True
+    def get_manager(self):
+        return self.ui_manager
+    def update(self):
+        tick = self.clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                self.running = False
+            self.ui_manager.process_events(event)
+        self.ui_manager.update(tick/1000)
+    def draw(self):
+        self.ui_manager.draw_ui(self.win)
+        pygame.display.update()
+        self.win.fill((0,0,0))
